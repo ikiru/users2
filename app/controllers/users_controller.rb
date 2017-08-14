d1b6036c5c97945f6befc3d6f302957d043d6a37
Jeff Winkler <jeff.a.winkler@gmail.com>
2017-08-14 18:38:09 +0000

    user features

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def create_user
@@ -18,7 +19,7 @@ class UsersController < ApplicationController
   def edit
   end
 
-  def delete
-    #code
-  end
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
 end
